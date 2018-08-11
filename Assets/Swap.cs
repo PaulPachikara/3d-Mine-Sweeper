ae42924ef5cfbb47264bdbd7fe6e410429824e22
PaulPachikara <39889062+PaulPachikara@users.noreply.github.com>
2018-08-10 17:28:17 -0700

    first commit

---
+++ Assets/Swap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swap : MonoBehaviour {
+
+    public GameObject reveal;
+
+    private void OnMouseDown()
+    {
+        Instantiate(reveal, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
